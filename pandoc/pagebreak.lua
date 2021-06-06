f958246b393361b37f28522d3624106088d54d45
Adam Gordon Bell <adam@corecursive.com>
2021-06-06 09:45:05 -0400

    mobi

--- pandoc/pagebreak.lua
+++ pandoc/pagebreak.lua
@@ -1,9 +1,6 @@
 function Para (el)
     if #el.content == 1 and el.content[1].text == "Pagebreak" then
-      return pandoc.RawBlock('latex', '\\pagebreak')
-    --   return pandoc.RawBlock('markdown', 'between')
-    --   return pandoc.RawBlock('markdown', '## End')
-    --   return pandoc.RawBlock('markdown', '# End')
+      return pandoc.RawBlock('markdown', '# End')
     end
   end
   
